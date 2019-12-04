9898bea8e1d73e8f0477fc812ebcbcbb1220a7e8
mahoneyjam <jmamahoney@gmail.com>
2019-12-03 23:22:40 -0600

    PyBank first 3 answers

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,7 +11,7 @@ with open(csvpath, newline='') as csvfile:
     #split data by commas
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    #print(csvreader)
+    print(csvreader)
 
   
     csv_header = next(csvreader)
@@ -19,9 +19,35 @@ with open(csvpath, newline='') as csvfile:
 
     data = list(csvreader)
     Total_Months = len(data)
-    print("Total_Months:" + " " + str(Total_Months))
+    print("Total Months:" + " " + str(Total_Months))
 
+    pAndL = []
+    differences = []
     
+    for i in range(len(data)):
+            #print(data[i][1])
+            #older = data[i+1][1]
+            #recent = data[i][1]
+            #diff = (int(older)-int(recent))
+            #differences.append(diff)
+            pAndL.append(int(data[i][1]))
+           
+      #      if i ==1 and i<85:
+     #              differences.append(pAndL[i+1]-pAndL[i])
+            
+    total = sum(pAndL)
+    print("Total:" + " " + str(total))
+    #print (differences)
+
+    lastMonth = (data[i][1])
+    #print (lastMonth)
+
+    firstMonth = (data[0][1])
+    #print (firstMonth)
+
+    avgChange = ((int(lastMonth)-int(firstMonth))/i)
+    print("Avg Change:" + "" + str(avgChange))
+
 
     #for row in csvreader:
            # print(row) testing to make sure I pulled the file correctly
