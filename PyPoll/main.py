6643eb9d822d11ea07cde5a7dc2491a4ebded06e
mahoneyjam <jmamahoney@gmail.com>
2019-12-10 00:26:35 -0600

    Updated with text file and formatting.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,16 @@
 import os
 import csv
 
-# csvpath = os.path.join('.', 'Resources', 'election_data.csv')
-
 csvpath = 'election_data.csv'
 
 Candidate = {}
+voteKey = list(Candidate.keys())
+voteCount = list(Candidate.values())
 
 #read with CSV file
 with open(csvpath, newline='') as csvfile:
    
-    # split data by commas
-    csvreader = csv.reader(csvfile, delimiter=',')
-
+    csvreader = csv.reader(csvfile, delimiter=',')  # split data by commas
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
@@ -22,45 +20,67 @@ with open(csvpath, newline='') as csvfile:
     for row in csvreader:
         Candidate[row[2]] = Candidate.get(row[2], 0) + 1
 
-    
-# print("Total Votes:" + str(totalVotes))      
- 
-  # for row in csvreader:
-  #      print(row) pull the file
+    voteKey = list(Candidate.keys())
+    voteCount = list(Candidate.values())    
+    sum = 0
+    for num in voteCount:
+        sum = sum + num
 
-voteKey = list(Candidate.keys())
-#print(voteKey)
-voteCount = list(Candidate.values())
-#print(voteCount)
 
-sum = 0
-for num in voteCount:
-    sum = sum +num
-#print(sum)
+    # print("Total Votes:" + str(sum))
 
+    khanDecimal = ((voteCount[0])/(sum)*100)
+    khanPercent = str('{:,.2f}'.format(khanDecimal))
+    correyDecimal = ((voteCount[1])/(sum)*100)
+    correyPercent = str('{:,.2f}'.format(correyDecimal))
+    liDecimal = ((voteCount[2])/(sum)*100)
+    liPercent = str('{:,.2f}'.format(liDecimal))
+    otDecimal = ((voteCount[3])/(sum)*100)
+    otPercent = str('{:,.2f}'.format(otDecimal))
+
+    # print(voteKey[0], khanPercent + "%", voteCount[0])
+    # print(voteKey[1], correyPercent + "%", voteCount[1])
+    # print(voteKey[2], liPercent + "%", voteCount[2])
+    # print(voteKey[3], otPercent + "%", voteCount[3])
+
+
+
+    maxValue = max(Candidate.values()) 
+    maxKeys = [k for k, v in Candidate.items() if v == maxValue] 
+    # print("Winner:" + " " + str(maxKeys))
+
+
+
+text_file = open("PyPoll_output_PBL.txt", "w") # create text file in same folder as main.py, assign variable to path
+print('Election Results')
+text_file.write('Election Results')
+print('-------------------------')
+text_file.write('-------------------------')
 print("Total Votes:" + str(sum))
-
-khanDecimal = ((voteCount[0])/(sum))
-# print "{0:.0%}".format(khanDecimal)
-khanPercent = khanDecimal*100
-# print(khanPercent)
-
-correyDecimal = ((voteCount[1])/(sum))
-correyPercent = correyDecimal*100
-liDecimal = ((voteCount[2])/(sum))
-liPercent = liDecimal*100
-otDecimal = ((voteCount[3])/(sum))
-otPercent = otDecimal*100
-#print(khanDecimal)
-
-
-print(voteKey[0], khanPercent, voteCount[0])
-print(voteKey[1], correyPercent, voteCount[1])
-print(voteKey[2], liPercent, voteCount[2])
-print(voteKey[3], otPercent, voteCount[3])
-
-
-
-maxValue = max(Candidate.values()) 
-maxKeys = [k for k, v in Candidate.items() if v == maxValue] 
+text_file.write("Total Votes:" + str(sum))
+print('-------------------------')
+text_file.write('-------------------------')
+print(voteKey[0], khanPercent + "%", voteCount[0])
+text_file.write(khanPercent + "%")
+print('-------------------------')
+text_file.write('-------------------------')
+print(voteKey[1], correyPercent + "%", voteCount[1])
+text_file.write(voteKey[1])
+text_file.write(correyPercent + "%")
+print('-------------------------')
+text_file.write('-------------------------')
+print(voteKey[2], liPercent + "%", voteCount[2])
+text_file.write(voteKey[2])
+text_file.write(liPercent + "%")
+print('-------------------------')
+text_file.write('-------------------------')
+print(voteKey[3], otPercent + "%", voteCount[3])
+text_file.write(voteKey[3])
+text_file.write(otPercent + "%")
+print('-------------------------')
+text_file.write('-------------------------')
 print("Winner:" + " " + str(maxKeys))
+text_file.write("Winner:" + " " + str(maxKeys))
+print('-------------------------')
+text_file.write('-------------------------')
+text_file.close() # if you open it, close it
--- PyPoll/tempCodeRunnerFile.py
+++ PyPoll/tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-print(voteKey[0], khanPercent, voteCount[0])
-print(voteKey[1], correyPercent, voteCount[1])
-print(voteKey[2], liPercent, voteCount[2])
-print(voteKey[3], otPercent, voteCount[3])
+text_file.write(khanPercent + "%")
